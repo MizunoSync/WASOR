b55cf06d5e92a3ddf3a4af8dd5eb63822a5fa8cd
MizunoSync <84451052+MizunoSync@users.noreply.github.com>
2026-07-16 15:06:47 -0300

    Update FullBrightMode.lua

--- Modules/Render/FullBrightMode.lua
+++ Modules/Render/FullBrightMode.lua
@@ -8,6 +8,7 @@ local UI = VH.UI
 local Lighting = Services.Lighting
 local originalAmbient = State.originalAmbient
 local originalOutdoor = State.originalOutdoor
+local originalClockTime = State.originalClockTime
 
 local Players = Services.Players
 local LP = Services.LP
@@ -71,4 +72,4 @@ local consoleLogs = State.consoleLogs
 local consoleLogsMap = State.consoleLogsMap
 
 
-registerModule("Render", "FullBright Mode", 440, 50, true, S.FullBright, function(v) S.FullBright = v; if v then Lighting.Ambient = Color3.new(1, 1, 1); Lighting.OutdoorAmbient = Color3.new(1, 1, 1) else Lighting.Ambient = originalAmbient; Lighting.OutdoorAmbient = originalOutdoor end; saveConfig() end)
+registerModule("Render", "FullBright Mode", 440, 50, true, S.FullBright, function(v) S.FullBright = v; if v then Lighting.Ambient = Color3.new(1, 1, 1); Lighting.OutdoorAmbient = Color3.new(1, 1, 1) else Lighting.Ambient = originalAmbient; Lighting.OutdoorAmbient = originalOutdoor; Lighting.ClockTime = originalClockTime or 14 end; saveConfig() end)
