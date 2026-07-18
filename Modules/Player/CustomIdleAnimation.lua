f5da2ad50cc36e4b455069d614ed607cd0bbdc32
MizunoSync <84451052+MizunoSync@users.noreply.github.com>
2026-07-18 02:51:00 -0300

    Add files via upload

--- Modules/Player/CustomIdleAnimation.lua
+++ Modules/Player/CustomIdleAnimation.lua
@@ -8,67 +8,15 @@ local UI = VH.UI
 local applyCustomIdle = Utils.applyCustomIdle
 local animPresets = State.animPresets
 
-local Players = Services.Players
 local LP = Services.LP
-local Mouse = Services.Mouse
-local Camera = Services.Camera
 
-local getChar = Utils.getChar
-local getHRP = Utils.getHRP
-local getHum = Utils.getHum
 local notify = Utils.notify
-local showToast = UI.showToast
-local updateHUDArrayList = UI.updateHUDArrayList
 local registerModule = UI.registerModule
 
-local addToggleOption = UI.addToggleOption
-local addSliderOption = UI.addSliderOption
-local addDropdownOption = UI.addDropdownOption
-local addKeybindOption = UI.addKeybindOption
 local addTextboxOption = UI.addTextboxOption
 local addButtonOption = UI.addButtonOption
-local addSectionHeader = UI.addSectionHeader
-local addInfoRowOption = UI.addInfoRowOption
-local addCustomFrameOption = UI.addCustomFrameOption
-local addScrollFeedOption = UI.addScrollFeedOption
-local getOrCreateWindow = UI.getOrCreateWindow
-local createFloatingWindow = UI.createFloatingWindow
 
 local saveConfig = VH.Config.saveConfig
-local loadConfig = VH.Config.loadConfig
-local saveFavorites = VH.Config.saveFavorites
-local loadFavorites = VH.Config.loadFavorites
-local logMessage = VH.Logger.logMessage
-
-local checkFriendship = Utils.checkFriendship
-local teleportToHRP = Utils.teleportToHRP
-local spectatePlayer = Utils.spectatePlayer
-local resetCameraToSelf = Utils.resetCameraToSelf
-local enableFreecam = Utils.enableFreecam
-local disableFreecam = Utils.disableFreecam
-local teleportToRandom = Utils.teleportToRandom
-local teleportToLowestPop = Utils.teleportToLowestPop
-local teleportToHighestPop = Utils.teleportToHighestPop
-local runExternalScript = Utils.runExternalScript
-local teleportToPlace = Utils.teleportToPlace
-
-local serverStatsLabels = State.serverStatsLabels
-local rowRegion = State.rowRegion
-local rowPing = State.rowPing
-local rowPlayers = State.rowPlayers
-local rowAge = State.rowAge
-
-local spectateStatsLabels = State.spectateStatsLabels
-local specNameRow = State.specNameRow
-local specHpRow = State.specHpRow
-local specTeamRow = State.specTeamRow
-
-local activeChatFeed = State.activeChatFeed
-local activeConsoleFeed = State.activeConsoleFeed
-
-local consoleLogs = State.consoleLogs
-local consoleLogsMap = State.consoleLogsMap
-
 
 registerModule("Player", "Custom Idle Animation", 160, 50, true, S.CustomIdleAnim, function(v) S.CustomIdleAnim = v; if LP.Character then applyCustomIdle(LP.Character) end; saveConfig() end, function(drawer)
     addTextboxOption(drawer, "Animation ID", "rbxassetid://507766666", function(txt) S.CustomIdleID = txt; if S.CustomIdleAnim and LP.Character then applyCustomIdle(LP.Character) end; saveConfig() end)
--- Modules/Player/ResetCharacter.lua
+++ Modules/Player/ResetCharacter.lua
@@ -1,71 +1,13 @@
 local VH = _G.VoidHub
-local Services = VH.Services
 local State = VH.State
 local S = State.S
 local Utils = VH.Utils
 local UI = VH.UI
 
-local Players = Services.Players
-local LP = Services.LP
-local Mouse = Services.Mouse
-local Camera = Services.Camera
-
 local getChar = Utils.getChar
-local getHRP = Utils.getHRP
 local getHum = Utils.getHum
 local notify = Utils.notify
-local showToast = UI.showToast
-local updateHUDArrayList = UI.updateHUDArrayList
 local registerModule = UI.registerModule
-
-local addToggleOption = UI.addToggleOption
-local addSliderOption = UI.addSliderOption
-local addDropdownOption = UI.addDropdownOption
-local addKeybindOption = UI.addKeybindOption
-local addTextboxOption = UI.addTextboxOption
-local addButtonOption = UI.addButtonOption
-local addSectionHeader = UI.addSectionHeader
-local addInfoRowOption = UI.addInfoRowOption
-local addCustomFrameOption = UI.addCustomFrameOption
-local addScrollFeedOption = UI.addScrollFeedOption
-local getOrCreateWindow = UI.getOrCreateWindow
-local createFloatingWindow = UI.createFloatingWindow
-
-local saveConfig = VH.Config.saveConfig
-local loadConfig = VH.Config.loadConfig
-local saveFavorites = VH.Config.saveFavorites
-local loadFavorites = VH.Config.loadFavorites
-local logMessage = VH.Logger.logMessage
-
-local checkFriendship = Utils.checkFriendship
-local teleportToHRP = Utils.teleportToHRP
-local spectatePlayer = Utils.spectatePlayer
-local resetCameraToSelf = Utils.resetCameraToSelf
-local enableFreecam = Utils.enableFreecam
-local disableFreecam = Utils.disableFreecam
-local teleportToRandom = Utils.teleportToRandom
-local teleportToLowestPop = Utils.teleportToLowestPop
-local teleportToHighestPop = Utils.teleportToHighestPop
-local runExternalScript = Utils.runExternalScript
-local teleportToPlace = Utils.teleportToPlace
-
-local serverStatsLabels = State.serverStatsLabels
-local rowRegion = State.rowRegion
-local rowPing = State.rowPing
-local rowPlayers = State.rowPlayers
-local rowAge = State.rowAge
-
-local spectateStatsLabels = State.spectateStatsLabels
-local specNameRow = State.specNameRow
-local specHpRow = State.specHpRow
-local specTeamRow = State.specTeamRow
-
-local activeChatFeed = State.activeChatFeed
-local activeConsoleFeed = State.activeConsoleFeed
-
-local consoleLogs = State.consoleLogs
-local consoleLogsMap = State.consoleLogsMap
-
 
 registerModule("Player", "Reset Character", 160, 50, false, false, function()
     if game.PlaceId == 286090429 then
@@ -75,19 +17,19 @@ registerModule("Player", "Reset Character", 160, 50, false, false, function()
 
     local char = getChar()
     local hum = getHum()
-    
+
     local hasGodMode = S.GodMode or S.GodModeConn or (hum and hum.MaxHealth > 99999)
     if hasGodMode then
         pcall(function() Utils.disableGodMode() end)
     end
-    
+
     if hum then
-        pcall(function() 
+        pcall(function()
             hum:SetStateEnabled(Enum.HumanoidStateType.Dead, true)
-            hum.Health = 0 
+            hum.Health = 0
         end)
     end
-    
+
     if char then
         local head = char:FindFirstChild("Head")
         if head then pcall(function() head:Destroy() end) end
@@ -97,6 +39,6 @@ registerModule("Player", "Reset Character", 160, 50, false, false, function()
         if torso then pcall(function() torso:Destroy() end) end
         pcall(function() char:BreakJoints() end)
     end
-    
+
     notify("Character reset!", Color3.fromRGB(218, 38, 38))
 end)
