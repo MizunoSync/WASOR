ddecf349a8cb56cba00854e62f305107568ca60b
MizunoSync <84451052+MizunoSync@users.noreply.github.com>
2026-07-20 00:35:12 -0300

    Update github_loader.lua

--- github_loader.lua
+++ github_loader.lua
@@ -97,6 +97,8 @@ local Modules = {
     "Modules/Render/FullBrightMode",
     "Modules/Render/TimeofDayCycle",
     "Modules/Render/FieldofView",
+    "Modules/Render/OutOfViewIndicators",
+    "Modules/Render/Minimap",
 
     -- World
     "Modules/World/InstantPrompts",
