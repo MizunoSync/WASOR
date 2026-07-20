93a1866da5d33cbdd8558a3a0e13048ae5553052
MizunoSync <84451052+MizunoSync@users.noreply.github.com>
2026-07-20 00:38:33 -0300

    Update FireTouchinterests.lua

--- Modules/World/FireTouchinterests.lua
+++ Modules/World/FireTouchinterests.lua
@@ -4,9 +4,15 @@ local S = State.S
 local UI = VH.UI
 
 local registerModule = UI.registerModule
-
+local addTextboxOption = UI.addTextboxOption
 local addSliderOption = UI.addSliderOption
-
 local saveConfig = VH.Config.saveConfig
 
-registerModule("World", "Fire touchinterests", 580, 50, true, S.TouchAura, function(v) S.TouchAura = v; saveConfig() end, function(drawer) addSliderOption(drawer, "Range (studs)", 5, 30, S.KillAuraRange or 15, function(v) S.KillAuraRange = v; saveConfig() end) end, false)
+registerModule("World", "Fire touchinterests", 580, 50, true, S.FireTouchinterestsActive, function(v)
+    S.FireTouchinterestsActive = v
+    saveConfig()
+end, function(drawer)
+    local tb = addTextboxOption(drawer, "Name Filter", "coin/checkpoint/etc", function(txt) S.FireTouchFilter = txt; saveConfig() end)
+    tb.Set(S.FireTouchFilter or "")
+    addSliderOption(drawer, "Max Distance (studs)", 5, 1000, S.FireTouchDistance or 100, function(v) S.FireTouchDistance = v; saveConfig() end)
+end, false)
