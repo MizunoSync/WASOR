c47553b34f796f1b8d0b691f96b0cbcd72783351
MizunoSync <84451052+MizunoSync@users.noreply.github.com>
2026-08-18 00:21:04 -0300

    Add files via upload

--- Modules/Combat/AutoClicker.lua
+++ Modules/Combat/AutoClicker.lua
@@ -22,8 +22,9 @@ registerModule("Combat", "Auto Clicker", 20, 50, true, S.AutoClicker, function(v
     local currentSession = clickerSession
     if v then
         task.spawn(function()
-            if S.AutoClickerDelay and S.AutoClickerDelay > 0 then
-                task.wait(S.AutoClickerDelay)
+            local delayTime = S.AutoClickerDelay or 2
+            if delayTime > 0 then
+                task.wait(delayTime)
             end
             while S.AutoClicker and State.uiRunning and clickerSession == currentSession do
                 if not Utils.isMouseOverHubUI() then
@@ -51,6 +52,6 @@ registerModule("Combat", "Auto Clicker", 20, 50, true, S.AutoClicker, function(v
     saveConfig()
 end, function(drawer)
     addKeybindOption(drawer, "Auto Clicker Bind", S.AutoClickerKey or Enum.KeyCode.Unknown, function(k) S.AutoClickerKey = k; saveConfig() end)
-    addSliderOption(drawer, "Start Delay (sec)", 0, 5, S.AutoClickerDelay or 1, function(v) S.AutoClickerDelay = v; saveConfig() end)
+    addSliderOption(drawer, "Start Delay (sec)", 0, 5, S.AutoClickerDelay or 2, function(v) S.AutoClickerDelay = v; saveConfig() end)
     addSliderOption(drawer, "Click Interval (sec)", 1, 100, math.round((S.AutoClickerInterval or 0.1) * 100), function(v) S.AutoClickerInterval = v / 100; saveConfig() end)
 end, false)
