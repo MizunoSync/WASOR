b9537aebd5e4d080da2159b4f8c117f179091119
MizunoSync <84451052+MizunoSync@users.noreply.github.com>
2026-08-18 00:24:03 -0300

    Add files via upload

--- Modules/World/AntiVoidNet.lua
+++ Modules/World/AntiVoidNet.lua
@@ -9,4 +9,4 @@ local addSliderOption = UI.addSliderOption
 
 local saveConfig = VH.Config.saveConfig
 
-registerModule("World", "Anti-Void Net", 580, 50, true, S.AntiVoid, function(v) S.AntiVoid = v; saveConfig() end, function(drawer) addSliderOption(drawer, "Anti-Void Height Y Offset", -2000, -100, S.AntiVoidY, function(v) S.AntiVoidY = v; saveConfig() end) end, false)
+registerModule("World", "Anti Void", 580, 50, true, S.AntiVoid, function(v) S.AntiVoid = v; saveConfig() end, function(drawer) addSliderOption(drawer, "Anti-Void Height Y Offset", -2000, -100, S.AntiVoidY, function(v) S.AntiVoidY = v; saveConfig() end) end, false)
--- Modules/World/ToolMagnet.lua
+++ Modules/World/ToolMagnet.lua
@@ -7,4 +7,4 @@ local registerModule = UI.registerModule
 
 local saveConfig = VH.Config.saveConfig
 
-registerModule("World", "Tool Magnet", 580, 50, true, S.ToolMagnet, function(v) S.ToolMagnet = v; saveConfig() end)
+registerModule("World", "Tool Magnet(Local)", 580, 50, true, S.ToolMagnet, function(v) S.ToolMagnet = v; saveConfig() end)
