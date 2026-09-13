8aa8be902da5f70c6a68cfb9d23b4fc1758783ef
MizunoSync <84451052+MizunoSync@users.noreply.github.com>
2026-09-13 09:54:40 -0300

    Add files via upload

--- Core/State.lua
+++ Core/State.lua
@@ -84,7 +84,7 @@ State.S = {
     LastSafePosition = CFrame.new(0, 50, 0), ChatHistory = {}, FavoriteMaps = {},
     FlyKey = Enum.KeyCode.Unknown, NoClipKey = Enum.KeyCode.Unknown, BHopKey = Enum.KeyCode.Unknown,
     InfJumpKey = Enum.KeyCode.Unknown, GhostKey = Enum.KeyCode.Unknown, BlinkKey = Enum.KeyCode.Unknown,
-    JumpStrengthKey = Enum.KeyCode.Unknown,
+    JumpStrengthKey = Enum.KeyCode.Unknown, WalkFlingKey = Enum.KeyCode.Unknown,
     ThemeColor = "Galaxy", ThemeGradientStyle = "Linear Gradient", ThemeGradientAngle = "Horizontal (0°)", CustomThemeHex = "#8D2FC4", GalaxyBgStyle = "Deep Space (Black)", GalaxyCustomBgHex = "#080810", TextAnimationStyle = "Static", HUDWatermark = true, HUDCoords = true, HUDArrayList = true, HUDArrayListOutside = true,
     MacroKey = Enum.KeyCode.Unknown, MacroText = "WeAreSkidding On Top!", UIToggleKey = Enum.KeyCode.RightControl,
     AimbotHoldMode = "M2", AimbotHoldKey = Enum.KeyCode.Unknown, HUDArrayListX = 10, HUDArrayListY = 70,
